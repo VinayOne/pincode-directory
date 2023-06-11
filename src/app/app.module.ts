5caa841442877dc5dec894d18e5d1dc033f1feec
vinayone <vinaymunda@outlook.com>
2023-06-11 20:42:11 +0530

    pincode in title for seo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +30,7 @@ import { VideoComponent } from './dashboard/video/video.component';
     BrowserAnimationsModule,
     SharedModulesModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/search-result/search-result.component.ts
+++ src/app/dashboard/search-result/search-result.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 import { CommonServiceService } from 'src/app/services/common-service.service';
 
 @Component({
@@ -13,7 +14,7 @@ export class SearchResultComponent {
   pincode: number = 110001;
   pincodeDetails: any;
 
-  constructor(private route: ActivatedRoute, private commonService: CommonServiceService) {
+  constructor(private route: ActivatedRoute, private commonService: CommonServiceService, private titleService: Title) {
     this.route.paramMap.subscribe( params => {
       this.pincodePassedData = params;
       this.pincode = this.pincodePassedData.params.pincode;
@@ -27,6 +28,8 @@ export class SearchResultComponent {
     } else {
       if(this.pincodePassedData) this.getPincodeDetails();
     }
+    console.log(this.pincodeDetails);
+    this.titleService.setTitle(`Pin Code ${this.pincode} ${this.pincodeDetails?.result[0]?.VillageLocality} ${this.pincodeDetails?.result[0]?.PostOffice} ${this.pincodeDetails?.result[0]?.State}`);
   }
 
   getPincodeDetails() {
