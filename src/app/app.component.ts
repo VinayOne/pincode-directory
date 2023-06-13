1c5c4f24682ba1977c495f41c806903df9bef47c
vinayone <vinaymunda@outlook.com>
2023-06-13 23:03:10 +0530

    excluded ip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component,Inject, PLATFORM_ID } from '@angular/core';
 import { CommonServiceService } from './services/common-service.service';
-import { Meta, Title } from '@angular/platform-browser';
 import {BehaviorSubject} from 'rxjs';
 import {isPlatformBrowser} from '@angular/common';
 
@@ -17,26 +16,15 @@ export class AppComponent {
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: any,
-    private commonService: CommonServiceService, 
-    private titleService: Title, private meta: Meta
+    private commonService: CommonServiceService
     ) {
       AppComponent.isBrowser.next(isPlatformBrowser(platformId));
       }
 
-  ngOnInit(): void{
-    this.meta.addTags([
-      {name: 'discription', content: 'Get your street or area Pin Code with this easy to use Pincode Directory! Find your current location Pin Code with this quick and easy online tool. Ex URL: https://pincode.directory/110054'},
-      {name: 'author' , content: 'Vinay Kumar Munda'},
-      {name: 'robots', content: 'index, follow'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {name: 'keywords', content: 'pincode, pincode of my location, current location pin code, my current location pin code, my area pin code'},
-      {name: 'date', content: '2023-06-13', scheme: 'YYYY-MM-DD'},
-      {charset: 'UTF-8'}
-    ]);
-    
+  ngOnInit(): void{    
     this.fetchVisitorDetails();
     setTimeout(() => {
-      if(this.visitorData.ip !== '125.99.240.100') {
+      if(this.visitorData.ip !== '125.99.240.100' || this.visitorData.ip !== '144.24.59.157') {
         this.captureVisitorDetails();
       } else {
         console.info('Developer Visit');
--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +10,18 @@ export class HomeComponent {
 
  pageTitle = 'Pincode Directory | Find Current Location Pin Code - Home';
 
- constructor(private titleService: Title) { }
+ constructor(private titleService: Title, private metaService: Meta) { }
 
  ngOnInit(): void{
+  this.metaService.addTags([
+    {name: 'discription', content: 'Get your street or area Pin Code with this easy to use Pincode Directory! Find your current location Pin Code with this quick and easy online tool. Ex URL: https://pincode.directory/110054'},
+    {name: 'author' , content: 'Vinay Kumar Munda'},
+    {name: 'robots', content: 'index, follow'},
+    {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+    {name: 'keywords', content: 'pincode, pincode of my location, current location pin code, my current location pin code, my area pin code'},
+    {name: 'date', content: '2023-06-13', scheme: 'YYYY-MM-DD'},
+    {charset: 'UTF-8'}
+  ]);
   this.titleService.setTitle(this.pageTitle);
  }
 
