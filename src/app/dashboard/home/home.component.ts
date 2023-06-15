010ddf93383f114d24a5cc672933bbc1c92f851d
vinayone <vinaymunda@outlook.com>
2023-06-15 19:17:26 +0530

    Meta tags & description optimized for seo

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -8,18 +8,18 @@ import { Title, Meta } from '@angular/platform-browser';
 })
 export class HomeComponent {
 
- pageTitle = 'Pincode Directory | Find Current Location Pin Code - Home';
+ pageTitle = 'Pin Code Directory | Find Current Location Pin Code';
 
  constructor(private titleService: Title, private metaService: Meta) { }
 
  ngOnInit(): void{
   this.metaService.addTags([
-    {name: 'description', content: 'Get your street or area Pin Code with this easy to use Pincode Directory! Find your current location Pin Code with this quick and easy online tool. Ex URL: https://pincode.directory/110054'},
+    {name: 'description', content: 'Get your street or area Pin Code with this easy to use Pin Code Directory! Find your current location Pin Code with this quick and easy online tool.'},
     {name: 'author' , content: 'Vinay Kumar Munda'},
     {name: 'robots', content: 'index, follow'},
     {name: 'viewport', content: 'width=device-width, initial-scale=1'},
     {name: 'keywords', content: 'pincode, pincode of my location, current location pin code, my current location pin code, my area pin code'},
-    {name: 'date', content: '2023-06-13', scheme: 'YYYY-MM-DD'},
+    {name: 'date', content: '2023-06-15', scheme: 'YYYY-MM-DD'},
     {charset: 'utf-8'}
   ]);
   this.titleService.setTitle(this.pageTitle);
