e194417e583ccb292cc9348dec49534344cfba7b
vinayone <vinaymunda@outlook.com>
2023-06-28 01:11:25 +0530

    multiple meta tags removed

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -18,13 +18,7 @@ export class HomeComponent {
 
  ngOnInit(): void{
   this.metaService.addTags([
-    {name: 'description', content: 'Get your area Pin Code with this easy-to-use Pin Code Directory! Search for current location pin code or pincode of my location with this quick & easy tool.'},
-    {name: 'author' , content: 'Vinay Kumar Munda'},
-    {name: 'robots', content: 'index, follow'},
-    {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-    {name: 'keywords', content: 'pin code, pincode of my location, current location pin code, my current location pin code, my area pin code'},
-    {name: 'date', content: '2023-06-15', scheme: 'YYYY-MM-DD'},
-    {charset: 'utf-8'}
+    {name: 'date', content: '2023-06-29', scheme: 'YYYY-MM-DD'}
   ]);
   this.titleService.setTitle(this.pageTitle);
  }
